2ade90905395a0085d9afd511440708084d6222d
kso000111 <kso000111@tukorea.ac.kr>
2025-11-06 01:18:52 +0900

    Fix: 누락된 코드 수정 적용

--- Assets/0_Scripts/Interface/ISaveHandler.cs
+++ Assets/0_Scripts/Interface/ISaveHandler.cs
@@ -1,7 +1,5 @@
 public interface ISaveHandler
 {
-    void RegisterSaveHandler() { Managers.Save.Register(this); }
-    void UnregisterSaveHandler() { Managers.Save.Unregister(this); }
     bool OnSaveRequest(GlobalDTO dto);
     bool OnLoadRequest(GlobalDTO dto);
 }
