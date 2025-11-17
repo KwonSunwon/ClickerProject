ebef539abe24ecf67a098059b36cdb6644c15247
mpmpm0610@gmail.com <>
2025-11-17 14:14:45 +0900

    Feat: 상점 창 닫기 버튼 추가

---
+++ Assets/0_Scripts/UI/Mine_Shop/Return_Button.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Return_Button : MonoBehaviour, IPointerClickHandler
+{
+	[SerializeField] GameObject Parent_GO;
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Destroy(Parent_GO);
+	}
+}
