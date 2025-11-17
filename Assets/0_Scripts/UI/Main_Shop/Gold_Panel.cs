6730df622c8e71a8170e75dfd885205e2f7367b9
mpmpm0610@gmail.com <>
2025-11-17 17:09:57 +0900

    Feat: 상점 판매 결과 골드에 적용

--- Assets/0_Scripts/UI/Main_Shop/Gold_Panel.cs
+++ Assets/0_Scripts/UI/Main_Shop/Gold_Panel.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 public class Gold_Panel : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI Gold_Text;
+
+	private void FixedUpdate()
+	{
+		Gold_Text.text = Managers.Stat.Gold.ToString();
+	}
 }
