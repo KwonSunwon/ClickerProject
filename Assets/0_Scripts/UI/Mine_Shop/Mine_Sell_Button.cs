50ac8cbe555b7b7edddb819d2253c87c5d1598ad
mpmpm0610@gmail.com <>
2025-11-17 13:59:39 +0900

    Feat: 광물 상점 인터페이스 완성

---
+++ Assets/0_Scripts/UI/Mine_Shop/Mine_Sell_Button.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Mine_Sell_Button : MonoBehaviour,IPointerClickHandler
+{
+    [SerializeField] TextMeshProUGUI Total_Gold_Text;
+    MineralSlot _slot;
+
+	public void Init(MineralSlot slot)
+    {
+        _slot = slot;
+		if (Total_Gold_Text != null)
+			Total_Gold_Text.text = $"{_slot.Amount}";
+
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		_slot.Amount -= _slot.Amount;
+	}
+
+	private void FixedUpdate()
+	{
+		if (Total_Gold_Text != null)
+			Total_Gold_Text.text = $"{_slot.Amount}";
+	}
+}
