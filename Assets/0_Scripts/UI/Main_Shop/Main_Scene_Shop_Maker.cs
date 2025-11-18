2bc9e074bb9785414aaf7680621def25bbb21d93
mpmpm0610@gmail.com <>
2025-11-18 15:11:15 +0900

    Feat: main shop 프리펩으로 추가가능하게 변경, 이미지 연동

---
+++ Assets/0_Scripts/UI/Main_Shop/Main_Scene_Shop_Maker.cs
@@ -0,0 +1,40 @@
+﻿using Steamworks;
+using UnityEngine;
+
+public class Main_Scene_Shop_Maker : MonoBehaviour
+{
+    [SerializeField] private int id;
+    void Start()
+    {
+		MakeShopPanel(id);
+	}
+
+    void Update()
+    {
+        
+    }
+
+	public void MakeShopPanel(int id)
+	{
+		string path = "Prefabs/UI/SubItem/Main_Secne_Shop_Panel";
+		GameObject prefab = Resources.Load<GameObject>(path);
+
+		if (prefab == null)
+		{
+			Debug.LogError($"Prefab not found at Resources/{path}");
+			return;
+		}
+
+		GameObject inst = Instantiate(prefab, transform);
+		var shopPanel = inst.GetComponent<Main_Secne_Shop_Panel>();
+
+		if (shopPanel == null)
+		{
+			Debug.LogError("Main_Scene_Shop_Panel component not found on instantiated prefab.");
+			return;
+		}
+
+		// 5. 자신의 id로 초기화
+		shopPanel.Init(id);
+	}
+}
---
+++ Assets/0_Scripts/UI/Main_Shop/Main_Secne_Shop_Panel.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main_Secne_Shop_Panel : MonoBehaviour
+{
+    int id;
+    [SerializeField] private Image Skill_Image;
+    [SerializeField] private TextMeshProUGUI Skill_Text;
+    [SerializeField] private TextMeshProUGUI Gold_Text;
+    
+    public void Init(int id)
+    {
+        this.id = id;
+		string path = $"Art/Main_Shop/Image_{id}";
+		Sprite sprite = Resources.Load<Sprite>(path);
+		Skill_Image.sprite = sprite;
+	}
+
+}
