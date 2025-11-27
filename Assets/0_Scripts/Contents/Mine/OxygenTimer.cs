e4cb729cbc44e73d39499612c471daf7bae402c1
kso000111@tukorea.ac.kr <>
2025-11-27 16:29:14 +0900

    Feat: 산소 타이머와 UI 게이지 연동 - 실시간으로 감소량 확인 가능

--- Assets/0_Scripts/Contents/Mine/OxygenTimer.cs
+++ Assets/0_Scripts/Contents/Mine/OxygenTimer.cs
@@ -4,36 +4,48 @@ using UnityEngine;
 
 public class OxygenTimer : MonoBehaviour
 {
-    private float _totalOxygen;
+    private float _totalOxygen = 300;
     private float _currentOxygen;
 
-    public float TotalOxygen { get; set; }
+    public float TotalOxygen {
+        get => _totalOxygen;
+        set => _totalOxygen = value;
+    }
 
     public event Action OnOxygenDepleted;
+    public event Action<float> OnOxygenChanged;
 
     private bool _isRunning = false;
 
     public void StartTimer(float totalOxygen = 0)
     {
+        if (_isRunning) {
+            Debug.LogWarning("@OxygenTimer - Oxygen timer is already running.");
+            return;
+        }
+
         if (totalOxygen > 0)
             _currentOxygen = totalOxygen;
         else
             _currentOxygen = _totalOxygen;
 
-        if (!_isRunning) {
-            _isRunning = true;
-            StartCoroutine(Timer(_currentOxygen));
-        }
-        else {
-            Debug.LogWarning("@OxygenTimer - Oxygen timer is already running.");
-        }
+        _isRunning = true;
+        StartCoroutine(Loop());
     }
 
-    private IEnumerator Timer(float time)
+    private IEnumerator Loop()
     {
-        yield return new WaitForSeconds(time);
+        while (_currentOxygen > 0) {
+            _currentOxygen -= Time.deltaTime;
+            OnOxygenChanged?.Invoke(_currentOxygen);
+            yield return null;
+        }
 
+        _currentOxygen = 0f;
+
+        OnOxygenChanged?.Invoke(_currentOxygen);
         OnOxygenDepleted?.Invoke();
+
         _isRunning = false;
     }
 }
