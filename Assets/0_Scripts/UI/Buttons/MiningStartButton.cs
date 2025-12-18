af41b50d5252b4256de436d4a55e204ba3b8dc5a
kso000111@tukorea.ac.kr <>
2025-12-18 14:06:24 +0900

    Feat: Cycle 횟수가 팝업으로 등장하도록 제작

--- Assets/0_Scripts/UI/Buttons/MiningStartButton.cs
+++ Assets/0_Scripts/UI/Buttons/MiningStartButton.cs
@@ -3,8 +3,14 @@ using UnityEngine.EventSystems;
 
 public class MiningStartButton : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private GameObject _cyclePanel;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         MineManager.Instance.StartMining();
+
+        if (_cyclePanel != null) {
+            Destroy(_cyclePanel);
+        }
     }
 }
